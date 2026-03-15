3fffa63de01c5eab4544bcc3f37b6b014afc8a36
pouyasadri <pouyapaghandeh78@gmail.com>
2026-03-15 12:36:15 +0100

    test: Add tests for peer connection establishment and error handling in New function

--- client/client_test.go
+++ client/client_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/pouyasadri/go-bittorrent/bitfield"
 	"github.com/pouyasadri/go-bittorrent/handshake"
 	"github.com/pouyasadri/go-bittorrent/message"
+	"github.com/pouyasadri/go-bittorrent/peers"
 	"net"
 	"testing"
 
@@ -208,4 +209,114 @@ func TestSendHave(t *testing.T) {
 	_, err = serverConn.Read(buf)
 	assert.Nil(t, err)
 	assert.Equal(t, expected, buf)
+}
+
+func TestNew(t *testing.T) {
+	ln, err := net.Listen("tcp", "127.0.0.1:0")
+	require.Nil(t, err)
+
+	done := make(chan struct{})
+	go func() {
+		defer ln.Close()
+		serverConn, err := ln.Accept()
+		if err != nil {
+			return
+		}
+
+		// 1. Read Handshake
+		buf := make([]byte, 68)
+		serverConn.Read(buf)
+
+		// 2. Write Handshake Back
+		serverHandshake := []byte{19, 66, 105, 116, 84, 111, 114, 114, 101, 110, 116, 32, 112, 114, 111, 116, 111, 99, 111, 108, 0, 0, 0, 0, 0, 0, 0, 0, 134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116, 45, 83, 89, 48, 48, 49, 48, 45, 192, 125, 147, 203, 136, 32, 59, 180, 253, 168, 193, 19}
+		serverConn.Write(serverHandshake)
+
+		// 3. Write Bitfield
+		bitfieldMsg := []byte{0x00, 0x00, 0x00, 0x06, 5, 1, 2, 3, 4, 5}
+		serverConn.Write(bitfieldMsg)
+
+		close(done)
+	}()
+
+	addr := ln.Addr().(*net.TCPAddr)
+	peer := peers.Peer{
+		IP:   addr.IP,
+		Port: uint16(addr.Port),
+	}
+
+	infoHash := [20]byte{134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116}
+	peerID := [20]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+
+	c, err := New(peer, peerID, infoHash, nil)
+	assert.Nil(t, err)
+	assert.NotNil(t, c)
+	<-done
+}
+
+func TestNewError(t *testing.T) {
+	// 1. Dial error (connection refused)
+	peer := peers.Peer{
+		IP:   net.ParseIP("127.0.0.1"),
+		Port: 12345, // Assuming this port is closed
+	}
+	_, err := New(peer, [20]byte{}, [20]byte{}, nil)
+	assert.NotNil(t, err)
+
+	// 2. Handshake error (server sends wrong bitfield or breaks connection)
+	ln, err := net.Listen("tcp", "127.0.0.1:0")
+	require.Nil(t, err)
+
+	go func() {
+		defer ln.Close()
+		conn, err := ln.Accept()
+		if err != nil {
+			return
+		}
+		// just close immediately
+		conn.Close()
+	}()
+
+	addr := ln.Addr().(*net.TCPAddr)
+	peer2 := peers.Peer{
+		IP:   addr.IP,
+		Port: uint16(addr.Port),
+	}
+
+	_, err = New(peer2, [20]byte{}, [20]byte{}, nil)
+	assert.NotNil(t, err)
+
+	// 3. Bitfield error
+	ln2, err := net.Listen("tcp", "127.0.0.1:0")
+	require.Nil(t, err)
+
+	go func() {
+		defer ln2.Close()
+		conn, err := ln2.Accept()
+		if err != nil {
+			return
+		}
+
+		// 1. Read Client Handshake
+		buf := make([]byte, 68)
+		conn.Read(buf)
+
+		// 2. Write Fake Handshake response
+		serverHandshake := []byte{19, 66, 105, 116, 84, 111, 114, 114, 101, 110, 116, 32, 112, 114, 111, 116, 111, 99, 111, 108, 0, 0, 0, 0, 0, 0, 0, 0, 134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116, 45, 83, 89, 48, 48, 49, 48, 45, 192, 125, 147, 203, 136, 32, 59, 180, 253, 168, 193, 19}
+		conn.Write(serverHandshake)
+
+		// close before sending bitfield
+		conn.Close()
+	}()
+
+	addr2 := ln2.Addr().(*net.TCPAddr)
+	peer3 := peers.Peer{
+		IP:   addr2.IP,
+		Port: uint16(addr2.Port),
+	}
+
+	infoHash := [20]byte{134, 212, 200, 0, 36, 164, 105, 190, 76, 80, 188, 90, 16, 44, 247, 23, 128, 49, 0, 116}
+	peerID := [20]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+
+	_, err = New(peer3, peerID, infoHash, nil)
+	assert.NotNil(t, err)
 }
